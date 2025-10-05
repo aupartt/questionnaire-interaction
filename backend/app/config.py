7bbbad18f326e6f8b581c83612cc8ee861b52739
Aurélien Partiot <aurelien.partiot@gmail.com>
2025-10-05 11:21:38 +0200

    Use config for API_KEY name and value

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,6 +4,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
+    # Const
+    API_KEY_NAME: str = "QUESTIONNAIRE_API_KEY"
+    MOCK_API_KEY: str = "bestapikey123"
+
+    # Base app settings
     api_base_path: str = "/api/v1"
     api_path_version: str = "v1"
     api_port: int = 8090
--- backend/app/middleware/api_key_middleware.py
+++ backend/app/middleware/api_key_middleware.py
@@ -1,17 +1,19 @@
 from fastapi import HTTPException, Request
 from starlette.middleware.base import BaseHTTPMiddleware
 
+from app.config import settings
+
 
 class APIKeyMiddleware(BaseHTTPMiddleware):
     def __init__(self, app):
         super().__init__(app)
-        self.valid_keys = ["test-api-key"]
+        self.valid_keys = [settings.MOCK_API_KEY]
 
     async def dispatch(self, request: Request, call_next):
-        api_key = request.headers.get("X-API-Key")
+        api_key = request.headers.get(settings.API_KEY_NAME)
         if not api_key or api_key not in self.valid_keys:
             raise HTTPException(status_code=401, detail="Invalid API Key")
 
-        request.state.user_api_key = api_key
+        request.state.api_key = api_key
         response = await call_next(request)
         return response
