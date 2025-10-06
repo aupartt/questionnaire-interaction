f7bc8f094fd2aeeb4e96199a38870a97321c6484
Aurélien Partiot <aurelien.partiot@gmail.com>
2025-10-06 16:14:22 +0200

    Add excluded_path to middleware

--- backend/app/middleware/api_key_middleware.py
+++ backend/app/middleware/api_key_middleware.py
@@ -8,8 +8,17 @@ class APIKeyMiddleware(BaseHTTPMiddleware):
     def __init__(self, app):
         super().__init__(app)
         self.valid_keys = [settings.MOCK_API_KEY]
+        self.excluded_paths = [
+            f"{settings.api_base_path}/docs",
+            "/redoc",
+            "/openapi.json",
+        ]
 
     async def dispatch(self, request: Request, call_next):
+        if request.url.path in self.excluded_paths:
+            response = await call_next(request)
+            return response
+
         api_key = request.headers.get(settings.API_KEY_NAME)
         if not api_key or api_key not in self.valid_keys:
             raise HTTPException(status_code=401, detail="Invalid API Key")
