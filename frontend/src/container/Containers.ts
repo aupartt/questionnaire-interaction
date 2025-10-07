f9260a674c2976d10a7789af5c9446ceddf29d55
Aurélien Partiot <aurelien.partiot@gmail.com>
2025-10-07 21:30:25 +0200

    feat: Add all usecases to Containers

--- frontend/src/container/Containers.ts
+++ frontend/src/container/Containers.ts
@@ -1,7 +1,25 @@
 import { AuthApiRepository } from '@/adapters/api/AuthApiRepository';
 import { VerifyApiKey } from '@/core/usecases/VerifyApiKey';
+import { GetAllQuestionnaire } from '@/core/usecases/GetAllQuestionnaire';
+import { QuestionnaireApiRepository } from '@/adapters/api/QuestionnaireApiRepository';
+import { GetSession } from '@/core/usecases/GetSession'
+import { SessionApiRepository } from '@/adapters/api/SessionApiRepository';
+import { SubmitAnswer } from '@/core/usecases/SubmitAnswer';
+import { AnswerApiRepository } from '@/adapters/api/AnswerApiRepository';
 
 
 export function getVerifyApiKeyUseCase(): VerifyApiKey {
     return new VerifyApiKey(new AuthApiRepository())
+}
+
+export function getAllQuestionnaireUseCase(): GetAllQuestionnaire {
+    return new GetAllQuestionnaire(new QuestionnaireApiRepository())
+}
+
+export function getSessionUseCase(): GetSession {
+    return new GetSession(new SessionApiRepository())
+}
+
+export function getSubmitAnswer(): SubmitAnswer {
+    return new SubmitAnswer(new AnswerApiRepository())
 }
