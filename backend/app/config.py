1c6f9b9f45a1798dd9acebdc4f088dc22099e17e
Aurélien Partiot <aurelien.partiot@gmail.com>
2025-10-08 19:05:25 +0200

    feat(backend): Initialize postgres and adminer

--- backend/app/config.py
+++ backend/app/config.py
@@ -14,12 +14,24 @@ class Settings(BaseSettings):
     # Const
     ENVIRONMENT: EnvironmentType = EnvironmentType.DEV
     API_KEY_MOCK: str = "foo"
+    API_KEY_NAME: str = "X-API-Key"
 
     # Base app settings
     API_BASE_PATH: str = "/api/v1"
     API_BASE_VERSION: str = "v1"
     API_PORT: int = 8090
     FRONT_URL: str = "*"
+
+    # Postgres
+    POSTGRES_HOST: str = "localhost"
+    POSTGRES_PORT: str = "5432"
+    POSTGRES_DB: str = "questionnaire"
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "postgres"
+
+    @property
+    def POSTGRES_URL(self) -> str:
+        return f"postgresql+psycopg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
 
 
 settings = Settings()
