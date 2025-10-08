b714110038228c6fb998ea85b875c5f2f7434a50
Aurélien Partiot <aurelien.partiot@gmail.com>
2025-10-08 20:52:41 +0200

    feat(backend): Add User schema

--- backend/app/schema/__init__.py
+++ backend/app/schema/__init__.py
@@ -2,6 +2,7 @@ from .answers import Answer, AnswerModel
 from .items import Item, ItemContent, ItemModel, ItemQuestion, ItemShort, NextItemResponse
 from .questionnaires import QuestionnaireModel, QuestionnaireStatus
 from .sessions import Session, SessionModel
+from .users import User
 
 __all__ = [
     "QuestionnaireModel",
@@ -16,4 +17,5 @@ __all__ = [
     "Session",
     "AnswerModel",
     "Answer",
+    "User",
 ]
