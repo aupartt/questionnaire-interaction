a86ce1ba57db9676004b9226dfa32e40c092ea6f
Aurélien Partiot <aurelien.partiot@gmail.com>
2025-10-08 22:42:37 +0200

    fix(backend): Bad user_id relation

--- backend/app/models/sessions.py
+++ backend/app/models/sessions.py
@@ -14,7 +14,6 @@ class SessionDB(Base):
     __tablename__ = "sessions"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    user_id: Mapped[str]
     status: Mapped[str]
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), nullable=False, default=lambda: datetime.now(UTC)
@@ -27,6 +26,7 @@ class SessionDB(Base):
     )
 
     # Relations
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
     questionnaire_id: Mapped[int] = mapped_column(ForeignKey("questionnaires.id", ondelete="CASCADE"))
     answers: Mapped[list["AnswerDB"]] = relationship(
         "AnswerDB",
