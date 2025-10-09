daca5ee8b35bb85b03804ea09d4b33b5a105031f
Aurélien Partiot <aurelien.partiot@gmail.com>
2025-10-09 03:17:35 +0200

    refactor(backend): Update change on controller

--- backend/app/controller/questionnaire_controller.py
+++ backend/app/controller/questionnaire_controller.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Depends
 
 from app.controller.dependencies.security import verify_api_key
-from app.controller.dependencies.services import questionnaire_service
 from app.schema import Answer, NextItemResponse, QuestionnaireStatus, Session
 from app.schema.common import VerifyResult, WillSmith
+from app.service import AnswerService, QuestionnaireService, ResultsService, SessionService
 from app.service.questionnaire_service import QuestionnaireService
 
 router = APIRouter()
@@ -11,9 +11,9 @@ router = APIRouter()
 
 @router.get("/questionnaires", response_model=list[QuestionnaireStatus])
 async def get_questionnaires(
-    api_key: str = Depends(verify_api_key), service: QuestionnaireService = Depends(questionnaire_service)
+    user_id: int = Depends(verify_api_key), service: QuestionnaireService = Depends(QuestionnaireService)
 ):
-    return await service.get_questionnaires(api_key=api_key)
+    return await service.get_questionnaires(user_id=user_id)
 
 
 @router.get("/verify", response_model=VerifyResult)
@@ -24,10 +24,10 @@ async def verify(_: str = Depends(verify_api_key)):
 @router.post("/questionnaire/{questionnaire_id}/session", response_model=Session)
 async def get_session(
     questionnaire_id: int,
-    api_key: str = Depends(verify_api_key),
-    service: QuestionnaireService = Depends(questionnaire_service),
+    user_id: int = Depends(verify_api_key),
+    service: SessionService = Depends(SessionService),
 ):
-    return await service.get_session(api_key=api_key, questionnaire_id=questionnaire_id)
+    return await service.get_session(user_id=user_id, questionnaire_id=questionnaire_id)
 
 
 @router.post("/questionnaire/{questionnaire_id}/session/{session_id}/answer", response_model=NextItemResponse)
@@ -36,11 +36,16 @@ async def add_answer(
     session_id: int,
     answer: Answer,
     _: str = Depends(verify_api_key),
-    service: QuestionnaireService = Depends(questionnaire_service),
+    service: AnswerService = Depends(AnswerService),
 ):
     return await service.add_answer(questionnaire_id=questionnaire_id, session_id=session_id, answer=answer)
 
 
 @router.post("/questionnaire/{questionnaire_id}/session/{session_id}/results", response_model=WillSmith)
-async def get_results(questionnaire_id: int, session_id: int, _: str = Depends(verify_api_key)):
-    return WillSmith(img_url="https://a.pinatafarm.com/540x494/76636b7956/tada-will-smith.jpg")
+async def get_results(
+    questionnaire_id: int,
+    session_id: int,
+    _: int = Depends(verify_api_key),
+    service: ResultsService = Depends(ResultsService),
+):
+    return await service.get_results(questionnaire_id=questionnaire_id, session_id=session_id)
