2606a571776d0ac4202d4ddd15ad00aa7f4da024
Aurélien Partiot <aurelien.partiot@gmail.com>
2025-10-11 14:36:41 +0200

    refactor(frontend): Add _old/api/ into new/

---
+++ frontend/src/app/new/api/session/results/route.ts
@@ -0,0 +1,32 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { getResultsApiUseCase } from "@/container/Containers";
+
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams;
+    const apiKey = searchParams.get("apiKey");
+    const questionnaireId = searchParams.get("questionnaireId");
+    const sessionId = searchParams.get("sessionId");
+
+    if (!apiKey || !questionnaireId || !sessionId) {
+        return NextResponse.json(
+            { error: "Paramètres non valide." },
+            { status: 400 },
+        );
+    }
+
+    try {
+        const getResults = getResultsApiUseCase();
+        const res = await getResults.execute(
+            apiKey,
+            parseInt(questionnaireId),
+            parseInt(sessionId),
+        );
+        return NextResponse.json(res);
+    } catch (error) {
+        console.error("Erreur API:", error);
+        return NextResponse.json(
+            { error: "Erreur lors de la récupération des résultats" },
+            { status: 500 },
+        );
+    }
+}
