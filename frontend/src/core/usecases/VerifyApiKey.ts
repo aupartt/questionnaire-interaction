55be5f218798d3f6835daf4070021ecb9c99b651
Aurélien Partiot <aurelien.partiot@gmail.com>
2025-10-11 14:31:44 +0200

    refactor(frontend): Update verify usecase to only return a boolean

--- frontend/src/core/usecases/VerifyApiKey.ts
+++ frontend/src/core/usecases/VerifyApiKey.ts
@@ -2,10 +2,10 @@ import type { ApiKeyStatus } from "../entities/ApiKeyStatus";
 import type { IAuthRepository } from "../ports/IAuthRepository";
 
 export class VerifyApiKey {
-    constructor(private readonly authRepository: IAuthRepository) {}
+    constructor(private readonly authRepository: IAuthRepository) { }
 
-    async execute(apiKey: string): Promise<ApiKeyStatus | null> {
+    async execute(apiKey: string): Promise<boolean> {
         const apiKeyStatus = await this.authRepository.verify(apiKey);
-        return apiKeyStatus;
+        return apiKeyStatus.isValid;
     }
 }
