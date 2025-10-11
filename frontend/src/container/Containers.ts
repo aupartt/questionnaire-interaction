85682522d7b3ac60e0c6827de16ad75d21fd98c7
Aurélien Partiot <aurelien.partiot@gmail.com>
2025-10-11 14:31:07 +0200

    refactor(frontend): Remove public adapter

--- frontend/src/container/Containers.ts
+++ frontend/src/container/Containers.ts
@@ -3,48 +3,28 @@ import { AuthApiRepository } from "@/adapters/api/AuthApiRepository";
 import { QuestionnaireApiRepository } from "@/adapters/api/QuestionnaireApiRepository";
 import { ResultsApiRepository } from "@/adapters/api/ResultsApiRepository";
 import { SessionApiRepository } from "@/adapters/api/SessionApiRepository";
-import { AnswerPubRepository } from "@/adapters/public/AnswerPubRepository";
-import { QuestionnairePubRepository } from "@/adapters/public/QuestionnairePubRepository";
-import { ResultsPubRepository } from "@/adapters/public/ResultsPubRepository";
-import { SessionPubRepository } from "@/adapters/public/SessionPubRepository";
 import { GetAllQuestionnaire } from "@/core/usecases/GetAllQuestionnaire";
 import { GetResults } from "@/core/usecases/GetResults";
 import { GetSession } from "@/core/usecases/GetSession";
 import { SubmitAnswer } from "@/core/usecases/SubmitAnswer";
 import { VerifyApiKey } from "@/core/usecases/VerifyApiKey";
 
-export function getVerifyApiKeyApiUseCase(): VerifyApiKey {
+export function getVerifyKeyUseCase(): VerifyApiKey {
     return new VerifyApiKey(new AuthApiRepository());
 }
 
-export function getAllQuestionnaireApiUseCase(): GetAllQuestionnaire {
+export function getAllQuestionnaireUseCase(): GetAllQuestionnaire {
     return new GetAllQuestionnaire(new QuestionnaireApiRepository());
 }
 
-export function getSessionApiUseCase(): GetSession {
+export function getSessionUseCase(): GetSession {
     return new GetSession(new SessionApiRepository());
 }
 
-export function getSubmitAnswerApiUseCase(): SubmitAnswer {
+export function getSubmitAnswerUseCase(): SubmitAnswer {
     return new SubmitAnswer(new AnswerApiRepository());
 }
 
-export function getResultsApiUseCase(): GetResults {
+export function getResultsUseCase(): GetResults {
     return new GetResults(new ResultsApiRepository());
-}
-
-export function getAllQuestionnairePubUseCase(): GetAllQuestionnaire {
-    return new GetAllQuestionnaire(new QuestionnairePubRepository());
-}
-
-export function getSessionPubUseCase(): GetSession {
-    return new GetSession(new SessionPubRepository());
-}
-
-export function getSubmitAnswerPubUseCase(): SubmitAnswer {
-    return new SubmitAnswer(new AnswerPubRepository());
-}
-
-export function getResultsPubUseCase(): GetResults {
-    return new GetResults(new ResultsPubRepository());
 }
