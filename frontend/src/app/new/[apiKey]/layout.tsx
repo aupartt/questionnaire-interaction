a600737c12c3db6a57295e755eda4bc32e2eeca7
Aurélien Partiot <aurelien.partiot@gmail.com>
2025-10-11 14:35:36 +0200

    feat(frontend): Verify API key on layer

--- frontend/src/app/new/[apiKey]/layout.tsx
+++ frontend/src/app/new/[apiKey]/layout.tsx
@@ -1,17 +1,27 @@
+import { getVerifyKeyUseCase } from "@/container/Containers";
 import { ThemeProvider } from "@/ui/contexts/ThemeContext";
+
+const VerifyApiKey = getVerifyKeyUseCase();
 
 export default async function ApiKeyLayout({
     children,
     params
-}: Readonly<{
+}: {
     children: React.ReactNode;
     params: Promise<{ apiKey: string }>
-}>) {
+}) {
     const { apiKey } = await params
+
+    const keyStatus = await VerifyApiKey.execute(apiKey)
+
+    if (!keyStatus) {
+        return <div>Clé API non valide.</div>
+    }
+
     return (
         <main>
             <ThemeProvider>
-            {children}
+                {children}
             </ThemeProvider>
         </main>
     );
