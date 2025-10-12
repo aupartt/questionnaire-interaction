900379706dfeb59c42a7ecfe91c1fcc831e9528e
Aurélien Partiot <aurelien.partiot@gmail.com>
2025-10-12 12:24:02 +0200

    refactor(frontend): Add SessionProvider

--- frontend/src/app/new/[apiKey]/questionnaires/[qId]/layout.tsx
+++ frontend/src/app/new/[apiKey]/questionnaires/[qId]/layout.tsx
@@ -1,29 +1,20 @@
-import { Tabs } from "@/ui/components/Tabs"
-import db from "@/lib/db";
-
+import { SessionProvider } from "@/ui/contexts/SessionContext";
 
 export default async function QuestionnaireLayout({
     children,
     params
-}: Readonly<{
+}: {
     children: React.ReactNode;
-    params: Promise<{ qId: number, apiKey: string }>
-}>) {
+    params: Promise<{ qId: string, apiKey: string }>
+}) {
     const { qId, apiKey } = await params
-
-    const items = db.getItems(qId)
-    const basePath = `/${apiKey}/questionnaires/${qId}/items`
 
     return (
         <main className="">
             <div className="flex h-screen">
-                <div className="w-1/3 border-r border-gray-200">
-                    <div className="h-10"></div>
-                    <h2 className="font-black text-2xl text-green-600 p-3">Aurélien</h2>
-                    <p className="text-sm pl-3">Aidez-nous à personnaliser votre expérience en nous parlant un peu de vous !</p>
-                    <Tabs basePath={basePath} items={items} />
-                </div>
-                {children}
+                <SessionProvider apiKey={apiKey} questionnaireId={parseInt(qId)}>
+                    {children}
+                </SessionProvider>
             </div>
         </main>
     );
