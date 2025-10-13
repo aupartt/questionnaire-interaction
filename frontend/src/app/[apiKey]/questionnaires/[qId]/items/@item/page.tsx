25f52d51b25316d54e9baf248d286e9344069a4c
Aurélien Partiot <aurelien.partiot@gmail.com>
2025-10-13 15:17:59 +0200

    refactor(frontend): Check for currentItem inside JSX

--- frontend/src/app/[apiKey]/questionnaires/[qId]/items/@item/page.tsx
+++ frontend/src/app/[apiKey]/questionnaires/[qId]/items/@item/page.tsx
@@ -11,13 +11,11 @@ export default function ApiKeyPage() {
     const { session, addAnswer } = useSessionContext();
     const router = useRouter();
 
-    if (!session) return "No session"; // For lint
-
     useEffect(() => {
-        if (session.status === "completed") {
+        if (session?.status === "completed") {
             router.push(`results`);
         }
-    }, [session]);
+    }, [session, router.push]);
 
     const submitAnswer = (answer: Answer) => {
         if (!answer) return;
@@ -28,12 +26,18 @@ export default function ApiKeyPage() {
         <div className="flex-1 p-4 overflow-auto">
             <div className="h-10"></div>
             <div className="flex flex-col item-center gap-7 mx-auto min-w-2xl w-1/2">
-                <ItemStepper />
-                <h2 className="font-bold">{session?.currentItem.name}</h2>
-                <ItemSection
-                    currentItem={session.currentItem}
-                    submit={submitAnswer}
-                />
+                {session?.currentItem && (
+                    <>
+                        <ItemStepper />
+                        <h2 className="font-bold">
+                            {session?.currentItem.name}
+                        </h2>
+                        <ItemSection
+                            currentItem={session?.currentItem}
+                            submit={submitAnswer}
+                        />
+                    </>
+                )}
             </div>
         </div>
     );
